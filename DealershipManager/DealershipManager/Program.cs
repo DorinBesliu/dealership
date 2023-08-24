15d3e257bbe431dd0837b1bf843d5091e6eebc25
BesliuDorin <besliudorin@gmail.com>
2023-08-24 22:18:00 +0300

    Add car management logic

--- DealershipManager/DealershipManager/Program.cs
+++ DealershipManager/DealershipManager/Program.cs
@@ -1,6 +1,15 @@
+using DealershipManager.Middleware;
+using DealershipManager.Repositories;
+using DealershipManager.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ExceptionHandlingMiddleware>();
+
+builder.Services.AddScoped<ICarService,CarService>();
+builder.Services.AddScoped<ICarRepository,InMemoryCarRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 
