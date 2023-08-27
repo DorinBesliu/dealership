37935e1ff2161ee4e6d805eff26e78dcb3a971dd
BesliuDorin <besliudorin@gmail.com>
2023-08-27 18:34:54 +0300

    Add client and sale management

--- DealershipManager/DealershipManager/Repositories/InMemoryCarRepository.cs
+++ DealershipManager/DealershipManager/Repositories/InMemoryCarRepository.cs
@@ -30,9 +30,9 @@ namespace DealershipManager.Repositories
             return _cars;
         }
 
-        public void Update(Guid carId, Car car)
+        public void Update(Car car)
         {
-            var carToUpdate = _cars.FirstOrDefault(c => c.Id == carId);
+            var carToUpdate = _cars.FirstOrDefault(c => c.Id == car.Id);
             if (carToUpdate != null)
             {
                 carToUpdate.Brand = car.Brand;
@@ -40,7 +40,7 @@ namespace DealershipManager.Repositories
                 carToUpdate.Category = car.Category;
                 carToUpdate.Price = car.Price;
                 carToUpdate.ProductionYear = car.ProductionYear;
-                carToUpdate.IsSold = car.IsSold;
+                
 
             }
         }
